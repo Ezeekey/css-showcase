218c76fc01a65049f5fe594b4ba6173c568fed9c
Zeke Taves <ezekielprof@gmail.com>
2023-05-01 22:01:38 -0500

    Implemented flashing borders.

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Where all the border styles for the flashing div are.
 const borders = ["solid", "dashed", "groove", "ridge", "dotted", "double", "inset", "outset"];
+
+// Variable to keep track of index in border array.
+let borderIndex = 0;
+
+
+function flashBorders () {
+    document.getElementById("flashingBorderDiv").style.borderStyle = borders[borderIndex];
+
+    borderIndex < borders.length ? borderIndex++ : borderIndex = 0;
+}
+
+
+// Launch border flasher
+setInterval(flashBorders, 1000);
